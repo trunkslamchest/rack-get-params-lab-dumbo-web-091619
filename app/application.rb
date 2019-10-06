86188000279a47fba9450eed7bf14b0ffb11bc38
Austin Smith <trunk9slamchest@gmail.com>
2019-10-06 23:01:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,8 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+		elsif req.path.match(/cart/)
+				@@cart.each { |item| resp.write "#{item}\n"}
     else
       resp.write "Path Not Found"
     end
